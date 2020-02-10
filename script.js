bb7cb95fc3beb16cd291750410b1b70a90909e0a
Matt Link <Linkm1993@gmail.com>
2020-02-09 21:25:31 -0500

    Added some arrays and got properly linked to the button

---
+++ script.js
@@ -0,0 +1,13 @@
+let button = document.getElementById('button');
+let display = document.getElementById('display');
+let password = [];
+let spchars = ["U+0021", "U+0022", "U+0023", "U+0024", "U+0025", "U+0026"];
+
+
+
+
+
+button.onclick = function() {
+    console.log("Poggers")
+    display.innerHTML = spchars
+}
