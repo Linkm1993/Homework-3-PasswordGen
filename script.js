fdc7ce0f9ef0df38f0c0a92c2e452aaaa9bc32f2
Matt Link <Linkm1993@gmail.com>
2020-02-13 14:31:25 -0500

    Added, check boxes, added mium password length of 8, defined handers in js, synced scroll bar and text input of password length, add basic on submit even for the password form

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-let button = document.getElementById('button');
-let display = document.getElementById('display');
-let password = [];
-let spchars = ["U+0021", "U+0022", "U+0023", "U+0024", "U+0025", "U+0026"];
+let slidebar = document.getElementById('slidebar')
+let numberInput = document.getElementById('numberinput')
+let passwordForm = document.getElementById('passwordForm')
+let includeUppercase = document.getElementById("uppercase")
+let includeLowercase = document.getElementById("lowercase")
+let includeNumbers = document.getElementById("includeNumbers")
+let includeSymbols = document.getElementById("includeSymbols")
+let passDisplay = document.getElementById("display")
 
 
 
+// Syncs the password length slide bar and number input
+numberinput.addEventListener('input', syncNumBar)
+slidebar.addEventListener('input', syncNumBar)
 
+passwordForm.addEventListener('submit', e => {
+    e.preventDefault()
+    let characterAmount = characterAmountNumber.value
+    let includeUppercase = includeUppercaseElement.checked
+    let includeNumbers = includeNumbersElement.checked
+    let includeSymbols = includeSymbolsElement.checked
+    let password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+    passDisplay.innerText = password
+  })
 
-button.onclick = function() {
-    console.log("Poggers")
-    display.innerHTML = spchars
+function syncNumBar(e) {
+    let value = e.target.value
+    slidebar.value = value
+    numberInput.value = value
 }
